b4e6f2f07d0f552f03c26dbb490e2af4a2b5548d
solvalle <vallesol07@gmail.com>
2022-06-11 22:59:40 -0600

    restore some models

---
+++ Planetarium/Models/QuizModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Planetarium.Models {
+    public class QuizModel {
+        [Display(Name = "Título")]
+        public string Title { get; set; }
+
+        [Display(Name = "Descripción")]
+        public string Description { get; set; }
+
+        [Display(Name = "Dificultad")]
+        public string Difficulty { get; set; }
+
+        [Display(Name = "Enlace")]
+        public string Link { get; set; }
+    }
+}
---
+++ Planetarium/Models/StreamingModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Planetarium.Models {
+    public class StreamingModel {
+        
+        [Required(ErrorMessage = "Es necesario que ingrese el enlace")]
+        [Display(Name = "Enlace del streaming")]
+        public string Link { get; set; }
+
+        [Display(Name = "Actividad Educativa")]
+        public string EducationalActivityTitle { get; set; }
+    }
+}
